5b635ff6141dbcdb9ac74752573e52e9fafb43fe
Yingjie.Lu <Yingjie.Lu@geely.com>
2019-08-08 17:23:03 +0800

    sdfsd

--- note.bat
+++ note.bat
@@ -31,11 +31,11 @@ exit
 :gitpull
 @cd C:\Users\yingjie.lu\Documents\note
 git pull
-@ if not '%flag%'=='open' (
-	@set /p isMerge=  '如果没有报错直接回车即可,如果报错则输入merge进行合并冲突'
-	@ if not '%isMerge%' == '' code C:\Users\yingjie.lu\Documents\note
+@ if '%flag%'=='open' (
+    code C:\Users\yingjie.lu\Documents\note
 ) else (
-	code C:\Users\yingjie.lu\Documents\note
+    @set /p isMerge=  '如果没有报错直接回车即可,如果报错则输入merge进行合并冲突'
+	@ if '%isMerge%' == '' code C:\Users\yingjie.lu\Documents\note
 )
 exit
 
