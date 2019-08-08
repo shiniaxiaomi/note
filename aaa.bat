ba40c1a4ea5417236dc81fd9e347688bcfd3723d
Yingjie.Lu <Yingjie.Lu@geely.com>
2019-08-08 17:27:23 +0800

    最终版

--- aaa.bat
+++
@@ -1,7 +0,0 @@
-
-@ if '%flag%'=='open' (
-    code C:\Users\yingjie.lu\Documents\note
-) else (
-    @set /p isMerge=  '如果没有报错直接回车即可,如果报错则输入merge进行合并冲突'
-	@ if '%isMerge%' == '' code C:\Users\yingjie.lu\Documents\note
-)
