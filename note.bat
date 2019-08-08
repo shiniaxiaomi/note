26762617a6420dd6fbcacf5ab38d5c8f9d19d17b
Yingjie.Lu <Yingjie.Lu@geely.com>
2019-08-08 17:02:01 +0800

    sdjfksd

--- note.bat
+++ note.bat
@@ -33,7 +33,7 @@ exit
 git pull
 @ if not '%flag%'=='open' (
 	@set /p isMerge=  '输入merge进行合并冲突'
-	@ if '%isMerge%' == 'merge' code C:\Users\yingjie.lu\Documents\note
+	@ if not '%isMerge%' == '' code C:\Users\yingjie.lu\Documents\note
 ) else (
 	code C:\Users\yingjie.lu\Documents\note
 )
