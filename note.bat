b4b8e4704edb5bc8439e8a63c77189b4ad9f7bc7
Yingjie.Lu <Yingjie.Lu@geely.com>
2019-10-28 13:39:21 +0800

    bc

--- note.bat
+++ note.bat
@@ -74,17 +74,17 @@ exit
 
 ::-----------以下为延时子程序--------------------
 :delay
-if "%1"=="" goto :eof
-set DelayTime=%1
-set TotalTime=0
-set NowTime=%time%
+@if "%1"=="" goto :eof
+@set DelayTime=%1
+@set TotalTime=0
+@set NowTime=%time%
 ::读取起始时间，时间格式为：13:01:05.95
 :delay_continue
-set /a minute1=1%NowTime:~3,2%-100
-set /a second1=1%NowTime:~-5,2%%NowTime:~-2%0-100000
-set NowTime=%time%
-set /a minute2=1%NowTime:~3,2%-100
-set /a second2=1%NowTime:~-5,2%%NowTime:~-2%0-100000
-set /a TotalTime+=(%minute2%-%minute1%+60)%%60*60000+%second2%-%second1%
-if %TotalTime% lss %DelayTime% goto delay_continue
-goto :eof
+@set /a minute1=1%NowTime:~3,2%-100
+@set /a second1=1%NowTime:~-5,2%%NowTime:~-2%0-100000
+@set NowTime=%time%
+@set /a minute2=1%NowTime:~3,2%-100
+@set /a second2=1%NowTime:~-5,2%%NowTime:~-2%0-100000
+@set /a TotalTime+=(%minute2%-%minute1%+60)%%60*60000+%second2%-%second1%
+@if %TotalTime% lss %DelayTime% goto delay_continue
+@goto :eof
